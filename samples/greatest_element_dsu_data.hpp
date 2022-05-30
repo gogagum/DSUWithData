d7c31f904c28e7377e692c01824f4a6d913827db
Georgy Guminov <gogagum@gmail.com>
2022-05-30 21:24:40 +0300

    Add new custom Data class test.

---
+++ samples/greatest_element_dsu_data.hpp
@@ -0,0 +1,37 @@
+//
+// Created by gogagum on 30.05.22.
+//
+
+#ifndef DSU_TEST_GREATEST_ELEMENT_DSU_DATA_HPP
+#define DSU_TEST_GREATEST_ELEMENT_DSU_DATA_HPP
+
+#include <functional>
+#include "../src/DefaultDSUData.hpp"
+
+template<class KeyT, class Comp = std::less<KeyT>>
+class GreatestElementSimpleDsuData : gdsu::BaseSimpleDSUData<KeyT> {
+public:
+    GreatestElementSimpleDsuData() : gdsu::BaseSimpleDSUData<KeyT>(KeyT{}) {};
+};
+
+template<class KeyT, class Comp = std::less<KeyT>>
+class GreatestElementRootDsuData : public gdsu::BaseRootDSUData<KeyT> {
+public:
+    explicit GreatestElementRootDsuData(const KeyT& key)
+            : gdsu::BaseRootDSUData<KeyT>(key), _size(1), _greatest(key) {};
+
+    GreatestElementSimpleDsuData<KeyT, Comp> joinWith(GreatestElementRootDsuData<KeyT, Comp>&& other) {
+        if (Comp()(_greatest, other._greatest)) {
+            _greatest = other._greatest;
+        }
+        return GreatestElementSimpleDsuData<KeyT, Comp>();
+    }
+
+    [[nodiscard]] const KeyT& getGreatest() const { return _greatest; };
+
+private:
+    KeyT _greatest;
+    std::size_t _size;
+};
+
+#endif //DSU_TEST_GREATEST_ELEMENT_DSU_DATA_HPP
--- test/custom_data_tests.cpp
+++ test/custom_data_tests.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 #include "../src/DSUWithData.hpp"
 #include "../samples/test_dsu_data.hpp"
+#include "../samples/greatest_element_dsu_data.hpp"
 
 TEST(CustomData, FloatAvgData) {
     auto dsu =
@@ -13,4 +14,16 @@ TEST(CustomData, FloatAvgData) {
     dsu.joinByKeys(2, 3);
 
     EXPECT_FLOAT_EQ(dsu.getRootData(2).getAvg(), 2.5);
+}
+
+TEST(CustomData, GreatestData) {
+    auto dsu =
+            gdsu::DSUWithData<int,
+                              GreatestElementRootDsuData<int>,
+                              GreatestElementSimpleDsuData<int>>(std::initializer_list<int>{1, 3, 6, 3, 5});
+
+    dsu.joinByKeys(6, 3);
+    dsu.joinByKeys(6, 1);
+
+    ASSERT_EQ(dsu.getComponent(3).getRootData().getGreatest(), 6);
 }
